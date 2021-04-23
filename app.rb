6aecb45aee221e193b94cd944b7328eec7afe05c
+ <+>
2021-04-23 10:52:51 -0700

    commit tree 2f85bee58d12be89e851066effcbf2035aff79e2

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Bucky Clark"
+    end
+
+    get '/hometown' do
+        "My hometown is Little Rock"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Chevrolet"
+    end
+
 end
